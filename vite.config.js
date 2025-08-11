3334d7a73cbe4a3b373dffdf0ea1b1bf7f6f57cf
Arbaz-khan786 <ak0297305@gmail.com>
2025-08-11 18:38:00 +0530

    Update homepage and base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/',  // <-- set this to '/'
+  base: '/Arbaz_PortFolio/',  // Use root base for Netlify
   plugins: [react()],
 });
+
